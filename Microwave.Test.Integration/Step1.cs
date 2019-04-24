68d9539c3cfd1ac88cd6410beace736a35cc91c7
Lauge Øster Vinther <lauge.oester.vinther@post.au.dk>
2019-04-24 11:29:55 +0200

    Step1 sat op

--- Microwave.Test.Integration/Step1.cs
+++ Microwave.Test.Integration/Step1.cs
@@ -3,19 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
 using NUnit.Framework;
+using NSubstitute;
 
 namespace Microwave.Test.Integration
 {
     [TestFixture]
     public class Step1
     {
-        private XXXXXXXXXXXXX sut;
+        private IUserInterface _UI;
+        private CookController _cookController;
+        private IDisplay _display;
+        private PowerTube _powerTube;
+        private Timer _timer;
+        private IOutput _output;
 
         [SetUp]
         public void SetUp()
         {
+            _UI = NSubstitute.Substitute.For<IUserInterface>();
+            _display = NSubstitute.Substitute.For<IDisplay>();
+            _output = NSubstitute.Substitute.For<IOutput>();
 
+            _timer = new Timer();
+            _powerTube = new PowerTube(_output); 
+            _cookController = new CookController(_timer, _display, _powerTube, _UI);
+        }
+
+        [Test]
+        public void Start__CookController_Timer_Powertube__50procent()
+        {
+            _cookController.StartCooking(50, 10);
+
+            Assert.That(_output.OutputLine())
         }
     }
 }
