ad6c231946a4bbdd4ecbd5443858248ed84a6596
Niels Stampe <niels.stampe@post.au.dk>
2019-04-24 11:35:55 +0200

    step 2 sat op

--- Microwave.Test.Integration/Step2.cs
+++ Microwave.Test.Integration/Step2.cs
@@ -3,18 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
 using NUnit.Framework;
+using NSubstitute;
 
 namespace Microwave.Test.Integration
 {
     class Step2
     {
-        private XXXXXXXXXXXXX sut;
+        private IUserInterface _UI;
+        private CookController _cookController;
+        private Display _display;
+        private PowerTube _powerTube;
+        private Timer _timer;
+        private IOutput _output;
 
         [SetUp]
         public void SetUp()
         {
+            _UI = NSubstitute.Substitute.For<IUserInterface>();
+            _output = NSubstitute.Substitute.For<IOutput>();
 
+            _display = new Display(_output);
+            _timer = new Timer();
+            _powerTube = new PowerTube(_output);
+            _cookController = new CookController(_timer, _display, _powerTube, _UI);
         }
     }
 }
