910e3da50a787466de8781b57748abef9b8b42ea
Niels Stampe <niels.stampe@post.au.dk>
2019-04-25 11:21:20 +0200

    step 3 opsat efter nyt skema

--- Microwave.Test.Integration/Step3.cs
+++ Microwave.Test.Integration/Step3.cs
@@ -13,21 +13,23 @@ namespace Microwave.Test.Integration
     class Step3
     {
         private UserInterface _UI;
-        private ICookController _cookController;
+        private CookController _cookController;
         private Light _light;
-        private IDisplay _display;
-        private IOutput _output;
-        private IDoor _door;
+        private Display _display;
+        private PowerTube _powerTube;
+
+        
         private IButton _powerButton;
         private IButton _timeButton;
         private IButton _startCancelButton;
+
+        private IOutput _output;
+        private IDoor _door;
 
         [SetUp]
         public void SetUp()
         {
             _output = NSubstitute.Substitute.For<IOutput>();
-            _cookController = NSubstitute.Substitute.For<ICookController>();
-            _display = NSubstitute.Substitute.For<IDisplay>();
             _door = NSubstitute.Substitute.For<IDoor>();
             _powerButton = NSubstitute.Substitute.For<IButton>();
             _powerButton = NSubstitute.Substitute.For<IButton>();
@@ -35,6 +37,9 @@ namespace Microwave.Test.Integration
             _startCancelButton = NSubstitute.Substitute.For<IButton>();
 
 
+            _cookController = new CookController(_display, _powerTube);
+            _display = new Display(_output);
+            _powerTube = new PowerTube(_output);
             _UI = new UserInterface(_powerButton, _timeButton, _startCancelButton, _door, _display, _light, _cookController);
             _light = new Light(_output);
         }
--- Microwave.Test.Integration/Step5.cs
+++ Microwave.Test.Integration/Step5.cs
@@ -3,18 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
 using NUnit.Framework;
 
 namespace Microwave.Test.Integration
 {
     class Step5
     {
-        private XXXXXXXXXXXXX sut;
+        private UserInterface _UI;
+        private ICookController _cookController;
+        private Light _light;
+        private IDisplay _display;
+        private IOutput _output;
+        private IDoor _door;
+        private IButton _powerButton;
+        private IButton _timeButton;
+        private IButton _startCancelButton;
 
         [SetUp]
         public void SetUp()
         {
+            _output = NSubstitute.Substitute.For<IOutput>();
+            _cookController = NSubstitute.Substitute.For<ICookController>();
+            _display = NSubstitute.Substitute.For<IDisplay>();
+            _door = NSubstitute.Substitute.For<IDoor>();
+            _powerButton = NSubstitute.Substitute.For<IButton>();
+            _powerButton = NSubstitute.Substitute.For<IButton>();
+            _timeButton = NSubstitute.Substitute.For<IButton>();
+            _startCancelButton = NSubstitute.Substitute.For<IButton>();
 
+
+            _UI = new UserInterface(_powerButton, _timeButton, _startCancelButton, _door, _display, _light, _cookController);
+            _light = new Light(_output);
         }
     }
 }
